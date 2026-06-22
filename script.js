62fdeafec6448fef379d9f418af54e445b25be9c
alpakamodus <michael.hoellrigl@hotmail.com>
2026-06-22 11:56:23 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ canvas.height = window.innerHeight;
 let x = canvas.width/2-canvas.width/4;
 let y = canvas.height/2-canvas.height/4;
 
+const boardSize = canvas.width/1.1;
+
 let lastTime = performance.now();
 
 let dragging = false;
@@ -47,7 +49,7 @@ function update(dt){
 function draw(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
-    ctx.fillRect(x-canvas.width/8,y-canvas.width/8,canvas.width/4,canvas.width/4);
+    ctx.fillRect(canvas.width/2-boardSize/2,canvas.height/2-boardSize/2-boardSize/4,canvas.width/4,canvas.width/4);
 }
 
 gameLoop();
