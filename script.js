5477f8ac5f20d105ca2d24e924b2d9abc68a3bc2
alpakamodus <michael.hoellrigl@hotmail.com>
2026-06-25 14:53:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ const board2X = boardX;
 const board2Y = boardY + boardSize + boardSize / 10;
 
 const blockY = board2Y + boardSize / 3 / 2;
-const block1X = board2X + boardSize / 4 * 1;
-const block2X = board2X + boardSize / 4 * 2;
-const block3X = board2X + boardSize / 4 * 3;
+const block1X = board2X + (boardSize / 4) * 1;
+const block2X = board2X + (boardSize / 4) * 2;
+const block3X = board2X + (boardSize / 4) * 3;
 
 const blockSize = boardSize / 8;
 
@@ -27,146 +27,264 @@ let lastTime = performance.now();
 
 let dragging = false;
 
-const blocks = [{
+const blockLib = [
+  [
+    //0
+    [1],
+  ],
+  [
+    //1
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+  ],
+  [
+    //2
+    [1, 0, 0],
+    [1, 1, 1],
+  ],
+  [
+    //3
+    [1, 1],
+    [0, 1],
+  ],
+  [
+    //4
+    [1, 1],
+    [1, 1],
+  ],
+  [
+    //5
+    [1, 1, 1],
+    [1, 0, 0],
+    [1, 0, 0],
+  ],
+  [
+    //6
+    [1, 1, 1],
+    [1, 0, 0],
+    [1, 0, 0],
+  ],
+  [
+    //7
+    [1, 1, 1, 1, 1],
+  ],
+  [
+    //8
+    [1, 1, 1, 1],
+  ],
+  [
+    //9
+    [1, 1, 1],
+    [1, 1, 1],
+  ],
+  [
+    //10
+    [1, 1, 1],
+    [1, 1, 1],
+  ],
+  [
+    //12
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+  [
+    //11
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+  [
+    //13
+    [1, 0],
+    [0, 1],
+  ],
+];
+
+const blocks = [
+  {
     homeX: block1X,
     homeY: blockY,
     x: block1X,
     y: blockY,
     placed: false,
-},
-{
+    Id: 1,
+  },
+  {
     homeX: block2X,
     homeY: blockY,
     x: block2X,
     y: blockY,
     placed: false,
-},
-{
+    Id: 1,
+  },
+  {
     homeX: block3X,
     homeY: blockY,
     x: block3X,
     y: blockY,
     placed: false,
-}];
+    Id: 1,
+  },
+];
 
-document.body.addEventListener("touchmove", (e) => {
+document.body.addEventListener(
+  "touchmove",
+  (e) => {
     e.preventDefault();
-}, { passive: false });
+  },
+  { passive: false },
+);
 
 canvas.addEventListener("pointerdown", (e) => {
-    blocks.forEach(b => {
-        if (e.clientX >= b.x - blockSize / 2 && e.clientX <= b.x + blockSize / 2 && e.clientY >= b.y - blockSize / 2 && e.clientY <= b.y + blockSize / 2 && !b.placed) {
-            b.x = e.clientX;
-            b.y = e.clientY;
-        }
-    });
-    dragging = true;
-    canvas.setPointerCapture(e.pointerId);
+  blocks.forEach((b) => {
+    if (
+      e.clientX >= b.x - blockSize / 2 &&
+      e.clientX <= b.x + blockSize / 2 &&
+      e.clientY >= b.y - blockSize / 2 &&
+      e.clientY <= b.y + blockSize / 2 &&
+      !b.placed
+    ) {
+      b.x = e.clientX;
+      b.y = e.clientY;
+    }
+  });
+  dragging = true;
+  canvas.setPointerCapture(e.pointerId);
 });
 canvas.addEventListener("pointerup", (e) => {
-
-    canvas.releasePointerCapture(e.pointerId);
-    dragging = false;
-    blocks.forEach(b => {
-        if (e.clientX >= b.x - blockSize / 2 && e.clientX <= b.x + blockSize / 2 && e.clientY >= b.y - blockSize / 2 && e.clientY <= b.y + blockSize / 2) {
-            b.placed = checkForSnap(b);
-        }
-    });
+  canvas.releasePointerCapture(e.pointerId);
+  dragging = false;
+  blocks.forEach((b) => {
+    if (
+      e.clientX >= b.x - blockSize / 2 &&
+      e.clientX <= b.x + blockSize / 2 &&
+      e.clientY >= b.y - blockSize / 2 &&
+      e.clientY <= b.y + blockSize / 2
+    ) {
+      b.placed = checkForSnap(b);
+    }
+  });
 });
 canvas.addEventListener("pointermove", (e) => {
-    if (dragging == true) {
-        blocks.forEach(b => {
-            if (e.clientX >= b.x - blockSize / 2 && e.clientX <= b.x + blockSize / 2 && e.clientY >= b.y - blockSize / 2 && e.clientY <= b.y + blockSize / 2 && !b.placed) {
-                b.x = e.clientX;
-                b.y = e.clientY;
-            }
-        });
-    }
+  if (dragging == true) {
+    blocks.forEach((b) => {
+      if (
+        e.clientX >= b.x - blockSize / 2 &&
+        e.clientX <= b.x + blockSize / 2 &&
+        e.clientY >= b.y - blockSize / 2 &&
+        e.clientY <= b.y + blockSize / 2 &&
+        !b.placed
+      ) {
+        b.x = e.clientX;
+        b.y = e.clientY;
+      }
+    });
+  }
 });
 
-const grid = Array(8).fill().map(() => Array(8).fill(0));
+const grid = Array(8)
+  .fill()
+  .map(() => Array(8).fill(0));
 
 function checkForSnap(b) {
-    for (let y = 0; y < 8; y++) {
-        for (let x = 0; x < 8; x++) {
-            if (Math.abs(b.x - (boardX + (x * blockSize + blockSize / 2))) <= blockSize / 2.1 && Math.abs(b.y - (boardY + (y * blockSize + blockSize / 2))) <= blockSize / 2.1 && !grid[y][x]) {
-                b.x = b.homeX;
-                b.y = b.homeY;
-                grid[y][x] = true;
-                placeCount++;
-                return true;
+  for (let y = 0; y < 8; y++) {
+    for (let x = 0; x < 8; x++) {
+      if (
+        Math.abs(b.x - (boardX + (x * blockSize + blockSize / 2))) <=
+          blockSize / 2.1 &&
+        Math.abs(b.y - (boardY + (y * blockSize + blockSize / 2))) <=
+          blockSize / 2.1 &&
+        !grid[y][x]
+      ) {
+        b.x = b.homeX;
+        b.y = b.homeY;
+        for (let h = 0; h < blockLib[b.Id].length; h++) {
+          for (let w = 0; w < blockLib[b.Id][0].length; w++) {
+            if(blockLib[b.Id][h][w]){
+                grid[y+h][x+w] = true;
             }
+          }
         }
+        placeCount++;
+        return true;
+      }
     }
-    b.x = b.homeX;
-    b.y = b.homeY;
-    return false;
+  }
+  b.x = b.homeX;
+  b.y = b.homeY;
+  return false;
 }
 
 function update(dt) {
-    if (placeCount >= 3) {
-        blocks.forEach(b => {
-            b.x = b.homeX;
-            b.y = b.homeY;
-            b.placed = false;
-        });
-        placeCount = 0;
-    }
+  if (placeCount >= 3) {
+    blocks.forEach((b) => {
+      b.x = b.homeX;
+      b.y = b.homeY;
+      b.placed = false;
+      b.Id = Math.floor(Math.random() * blockLib.length);
+    });
+    placeCount = 0;
+  }
 }
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //playing board outline
-    ctx.strokeStyle = "rgba(0, 0, 0, 1)";
-    ctx.lineWidth = 5;
-    ctx.strokeRect(boardX, boardY, boardSize, boardSize);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //playing board outline
+  ctx.strokeStyle = "rgba(0, 0, 0, 1)";
+  ctx.lineWidth = 5;
+  ctx.strokeRect(boardX, boardY, boardSize, boardSize);
 
-    //playing board karo
-    ctx.strokeStyle = "rgba(0, 0, 0, 0.1)";
-    ctx.lineWidth = 1;
-    for (let i = 1; i < 8; i++) {
-        ctx.beginPath();
-        ctx.moveTo(boardX + blockSize * i, boardY);   // Startpunkt
-        ctx.lineTo(boardX + blockSize * i, boardY + boardSize); // Endpunkt
-        ctx.stroke();
+  //playing board karo
+  ctx.strokeStyle = "rgba(0, 0, 0, 0.1)";
+  ctx.lineWidth = 1;
+  for (let i = 1; i < 8; i++) {
+    ctx.beginPath();
+    ctx.moveTo(boardX + blockSize * i, boardY); // Startpunkt
+    ctx.lineTo(boardX + blockSize * i, boardY + boardSize); // Endpunkt
+    ctx.stroke();
 
-        ctx.beginPath();
-        ctx.moveTo(boardX, boardY + blockSize * i);   // Startpunkt
-        ctx.lineTo(boardX + boardSize, boardY + blockSize * i); // Endpunkt
-        ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(boardX, boardY + blockSize * i); // Startpunkt
+    ctx.lineTo(boardX + boardSize, boardY + blockSize * i); // Endpunkt
+    ctx.stroke();
+  }
+  //2nd board outline
+  ctx.strokeStyle = "rgba(0, 0, 0, 1)";
+  ctx.lineWidth = 2;
+  ctx.strokeRect(board2X, board2Y, boardSize, boardSize / 3);
+
+  //blocks
+  ctx.fillStyle = "rgba(0, 0, 0, 1)";
+  for (let y = 0; y < 8; y++) {
+    for (let x = 0; x < 8; x++) {
+      if (grid[y][x] == true) {
+        ctx.fillRect(
+          boardX + x * blockSize,
+          boardY + y * blockSize,
+          blockSize,
+          blockSize,
+        );
+      }
     }
-    //2nd board outline
-    ctx.strokeStyle = "rgba(0, 0, 0, 1)";
-    ctx.lineWidth = 2;
-    ctx.strokeRect(board2X, board2Y, boardSize, boardSize / 3);
-
-    //blocks
-    ctx.fillStyle = "rgba(0, 0, 0, 1)";
-    for (let y = 0; y < 8; y++) {
-        for (let x = 0; x < 8; x++) {
-            if (grid[y][x] == true) {
-                ctx.fillRect(
-                    boardX + x * blockSize,
-                    boardY + y * blockSize,
-                    blockSize,
-                    blockSize
-                );
-            }
-        }
+  }
+  blocks.forEach((b) => {
+    if (b.placed == false) {
+      ctx.fillRect(
+        b.x - blockSize / 2,
+        b.y - blockSize / 2,
+        blockSize,
+        blockSize,
+      );
     }
-    blocks.forEach(b => {
-        if (b.placed == false) {
-            ctx.fillRect(b.x - blockSize / 2, b.y - blockSize / 2, blockSize, blockSize);
-        }
-    });
+  });
 }
 
 function gameLoop() {
-    let currentTime = performance.now();
-    let dt = (currentTime - lastTime) / 16.6667;
-    lastTime = currentTime;
-    update(dt);
-    draw();
-    requestAnimationFrame(gameLoop);
+  let currentTime = performance.now();
+  let dt = (currentTime - lastTime) / 16.6667;
+  lastTime = currentTime;
+  update(dt);
+  draw();
+  requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
